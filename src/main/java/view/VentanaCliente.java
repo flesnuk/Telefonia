5f4cb1a5a9cd6dc15d5f7f097a138b278bc1411b
Dinu <Dinu@192.168.1.130>
2016-05-01 22:49:49 +0200

    .

--- src/main/java/view/VentanaCliente.java
+++ src/main/java/view/VentanaCliente.java
@@ -86,12 +86,13 @@ public class VentanaCliente extends JPanel{
 		tabla.setModel(tablaClientes);
 		if (clientes==null) return;
 		Object[] col = new Object[3];
-		for (Llamada l : clientes){			
+		for (Llamada l : clientes){
+			if (l!=null){ 
 			col[0]=l.getTelefono();
 			col[1]=l.getDuracion();
 			col[2]=FechaToString.toString(l.getFecha());
 			((DefaultTableModel) tabla.getModel()).addRow(col);
-			
+			}
 		}
 	}
 	
